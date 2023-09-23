93a3757d153447b20083f13d6f7e19509c5afa91
Hannes Albert <hannes10042004@icloud.com>
2023-09-23 18:13:06 +0200

    1048

---
+++ 1048.java
@@ -0,0 +1,29 @@
+class Solution {
+  public int longestStrChain(String[] words) {
+    Set<String> wordsSet = new HashSet<>(Arrays.asList(words));
+    int ans = 0;
+
+    for (final String word : words)
+      ans = Math.max(ans, longestStrChain(word, wordsSet));
+
+    return ans;
+  }
+ 
+  public Map<String, Integer> dp = new HashMap<>();
+
+  public int longestStrChain(final String s, Set<String> wordsSet) {
+    if (dp.containsKey(s))
+      return dp.get(s);
+
+    int ans = 1;
+
+    for (int i = 0; i < s.length(); ++i) {
+      String pred = s.substring(0, i) + s.substring(i + 1);
+      if (wordsSet.contains(pred))
+        ans = Math.max(ans, longestStrChain(pred, wordsSet) + 1);
+    }
+
+    dp.put(s, ans);
+    return ans;
+  }
+}
