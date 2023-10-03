753e48dd0c7e759c186f7f08f720919307c3c8b2
Hannes Albert <hannes10042004@icloud.com>
2023-10-03 10:38:09 +0200

    Added 1512

---
+++ 1512.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+class Solution {
+public:
+  int numIdenticalPairs(std::vector<int>& nums) {
+        int counter = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = i + 1; j < nums.size(); j++) {
+                if(nums[i] == nums[j]) counter++;
+            }
+        }
+        return counter;
+    }
+};
